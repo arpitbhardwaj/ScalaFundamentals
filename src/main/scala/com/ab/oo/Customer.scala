57a07f704afe711dd877213d71d78f4d41dfa83b
Arpit Bhardwaj <arpitbhardwaj09@gmail.com>
2021-11-22 10:08:08 +0530

    trait and self type

--- src/main/scala/com/ab/oo/Customer.scala
+++ src/main/scala/com/ab/oo/Customer.scala
@@ -2,7 +2,7 @@ package com.ab.oo
 
 import java.util.UUID
 
-final class Customer (first:String, last:String, email:String){
+/*final*/ class Customer (first:String, last:String, email:String){
   private val _id:UUID = UUID.randomUUID()
   private val _first:String = first
   private val _last:String = last
@@ -16,7 +16,47 @@ final class Customer (first:String, last:String, email:String){
 
 //class HackCustomer (first:String = "**",last:String="**",email:String="**") extends Customer{}
 
+trait Address{
+  protected var _zip:Int
+  protected var _street:String
+  protected var _state:String
+
+  def setZip(zip:Int):Unit
+  def setStreet(street:String):Unit
+  def setState(state:String):Unit
+  def printAddress():Unit = println(s"CustomerWithAddress(${_zip}, ${_street}, ${_state})")
+}
+trait SecretCodeGenerator{
+  def getOTP:String = UUID.randomUUID().toString
+}
+
+class DepositBoxContainer extends SecretCodeGenerator
+
+class CustomerWithAddress(first:String, last:String, email:String)
+  extends Customer(first,last,email)
+    with Address
+    with SecretCodeGenerator {
+  override protected var _zip: Int = -1
+  override protected var _street: String = ""
+  override protected var _state: String = ""
+
+  override def setZip(zip: Int): Unit = _zip = zip
+
+  override def setStreet(street: String): Unit = _street = street
+
+  override def setState(state: String): Unit = _state = state
+}
 object CustomerRunner extends App{
   val c1:Customer = new Customer("Tony", "Stark", "tonystark@com")
   println(c1.getFirst,c1.getLast,c1.getEmail)
+
+  var c2:CustomerWithAddress = new CustomerWithAddress("Tony", "Stark", "tonystark@com")
+  c2.setZip(560048)
+  c2.setStreet("45 Fermont")
+  c2.setState("Karnataka")
+  c2.printAddress()
+  println(c2.getOTP)
+
+  val depositBoxContainer = new DepositBoxContainer
+  println("DepositBoxContainer OTP: " + depositBoxContainer.getOTP)
 }
---
+++ src/main/scala/com/ab/oo/Transaction.scala
@@ -0,0 +1,38 @@
+package com.ab.oo
+
+
+trait User{
+  def getFirst:String
+  def getLast:String
+}
+/*trait Transaction extends User {
+  def printAmountWithTransaction(amount:Double):Unit = {
+    val fullCustomerName = getFirst +" " + getLast
+    val tax = amount*0.10
+    println(s"$fullCustomerName paid $tax on amount $amount")
+  }
+}*/
+
+trait Transaction {
+  this:User=>
+  def printAmountWithTransaction(amount:Double):Unit = {
+    val fullCustomerName = this.getFirst +" " + this.getLast
+    val tax = amount*0.10
+    println(s"$fullCustomerName paid $tax on amount $amount")
+  }
+}
+
+class DebitTransaction(val first:String, val last:String) extends User with Transaction {
+  override def getFirst: String = first
+
+  override def getLast: String = last
+}
+
+object TransactionRunner extends App{
+  val transaction = new DebitTransaction("Tony","Stark")
+  transaction.printAmountWithTransaction(999)
+}
+
+
+
+
