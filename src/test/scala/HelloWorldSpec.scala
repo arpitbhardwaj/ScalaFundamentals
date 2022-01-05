ed573ce5e2772bb4e216b2aca554a374d8c28ff8
arpitbhardwaj <arpitbhardwaj09@gmail.com>
2022-01-05 11:15:03 +0530

    Unit Testing using ScalaTest

---
+++ src/test/scala/HelloWorldSpec.scala
@@ -0,0 +1,8 @@
+import org.scalatest.flatspec.AnyFlatSpec
+
+class HelloWorldSpec extends AnyFlatSpec{
+  behavior of "Hello World"
+  it should "start with 'Hello'" in {
+    assert("Hello World".startsWith("Hello"))
+  }
+}
